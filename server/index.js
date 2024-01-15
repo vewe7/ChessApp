7996f4f16fb02c03750384ad6db28fb352fa0976
vewe7 <naomi.yoon.do@gmail.com>
2024-01-14 23:44:21 -0500

    Add module for connecting database to server

--- server/index.js
+++ server/index.js
@@ -1,7 +1,8 @@
 const express = require("express")
+const pool = require("./db");
+const cors = require("cors");
 const app = express();
 const PORT = 5000;
-const cors = require("cors");
 
 app.use(cors());
 app.use(express.json());
@@ -9,3 +10,5 @@ app.use(express.json());
 app.listen(PORT, () => {
     console.log("server has started on port " + PORT);
 });
+
+// ROUTES 
