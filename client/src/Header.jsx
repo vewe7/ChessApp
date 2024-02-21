3b0ad5b1a25ab4875803af24c1a186f086a0447e
root <root@DESKTOP-Q99TUT6>
2024-02-21 13:37:55 -0500

    Added logo to the Header

--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -5,7 +5,9 @@ function Header() {
         <div className="Header">
             <h1 className="Title">FAFOChess</h1>
             <b2>Free And Fun Online Chess</b2>
+            <img class="logo" src ="client/FAFOCHESS.svg"></img>
         </div>
+        
     )
 }
 
