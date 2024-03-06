4cdf5efdec8262674b1ed8b9391e9aa763254877
vewe7 <naomi.yoon.do@gmail.com>
2024-03-06 14:16:11 -0500

    Add map to keep track of invites on backend

--- server/socket.js
+++ server/socket.js
@@ -1,4 +1,6 @@
 const db = require("./db-access");
+const invites = new Map();
+let inviteIterator = 1;
 
 function socketInitialize (io) {
   io.on("connection", async (socket) => {
@@ -14,6 +16,7 @@ function socketInitialize (io) {
     });
 
     socket.on("invite", async (to) => {
+      // Validate username
       const recipientUser = await db.getUserByUsername(to);
       if (recipientUser == null) {
         socket.emit("invite", "User not found");
@@ -22,26 +25,38 @@ function socketInitialize (io) {
         socket.emit("invite", "Cannot invite self");
         return;
       }
+      // Check if user is online
       const recipientSockets = await io.in(`user:${userId}`).fetchSockets();
       if (recipientSockets.length == 0) {
         socket.emit("invite", "User not online");
         return;
       }
+      // Create and send invite
       console.log(`user ${userId} is inviting user ${recipientUser.id}`);
-      io.to(`user:${recipientUser.id}`).emit("invite-ask", `User ${username} has invited you`, userId);
+
+      invites.set(inviteIterator, {inviter: userId, recipient: recipientUser.id});
+      io.to(`user:${recipientUser.id}`).emit("invite-ask", `User ${username} has invited you`, inviteIterator);
+      inviteIterator++;
       socket.emit("invite", "Invite sent");
+    });
+
+    socket.on("invite-answer", async (answer, inviteId) => {
+      // Check that invite id is valid
+      if (!invites.has(inviteId)) {
+        socket.emit("invite-answer", "Invalid invite id");
+        return;
+      }
+      if (answer == "accept") {
+        console.log("User accepted invite with id " + inviteId);
+        // Do accept stuff
+      } else { 
+        console.log("User declined invite with id " + inviteId);
+        // Do decline stuff
+      }
     });
 
     socket.on("disconnect", () => {
       console.log("User disconnected");
-    });
-
-    socket.on("invite-answer", async (answer, inviterId) => {
-      if (answer == "accept") {
-        console.log("User accepted invite from id " + inviterId);
-      } else { 
-        console.log("User declined invite from id " + inviterId);
-      }
     });
   });
 };
