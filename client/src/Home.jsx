f68e88af4ac7f985c743bcb24dbe2f76631c6ab0
vewe7 <naomi.yoon.do@gmail.com>
2024-04-10 12:58:14 -0400

    Fix deprecated useEffect in Home component

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -3,49 +3,14 @@ import { useState, useEffect } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import Logout from "./Logout";
+import Invite from "./Invite";
 import Header from "./Header";
-import io from "socket.io-client";
-
-const SOCKET_SERVER_URL = "http://localhost:5000";
 
 const Home = () => {
-  const [count, setCount] = useState(0);
-
-  useEffect(() => {
-    // Connect to socket server
-    const socket = io(SOCKET_SERVER_URL);
-
-    // socket.on('message', handleMessage);
-
-    // Cleanup on component unmount
-    return () => {
-      socket.disconnect();
-    };
-  }, []); // Empty array means this effect runs only once on mount
-
   return (
     <>
       <Header className="Header" />
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Invite />
       <Logout />
     </>
   );
